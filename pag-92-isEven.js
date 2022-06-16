cbc190d5a83c0b88c3d0ebe53966274bc54b6bf4
anderson-mntn <88107035+anderson-mntn@users.noreply.github.com>
2022-06-16 17:35:43 -0300

    Add files via upload

--- pag-92-isEven.js
+++ pag-92-isEven.js
@@ -1,5 +1,35 @@
-const isEven = (x) => x % 2 === 0;
+const isEven = x => x % 2 === 0;
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
+//map() 
 const myMap = numbers.map(isEven)
 console.log(myMap);
+
+//filter()
+const evenNumbers = numbers.filter(isEven)
+console.log(evenNumbers);
+
+//reduce() functions that receive the following parameters:
+//proviousValue, currentValue, index and array are optinals.
+//Podemos usar para devolver um valor que será somado a um acumulador
+//o qual será devolvido depois que o método parar de executar.
+
+//Useful for summing all array items:
+numbers.reduce((previous, current) => previous + current)
+
+//Map, Filter and Reduce the base of functional programing js.
+
+//Force pass through all elements
+let initialValue = 0
+let obj = [
+    
+    {name: 'asd', age:20 },
+    {name: 'qwe', age:20 },
+    {name: 'zxc', age:20 }
+    
+]
+let sum = obj.reduce(function(accumulator, curValue){
+    return accumulator + curValue.age
+}, initialValue);
+
+console.log(sum);
